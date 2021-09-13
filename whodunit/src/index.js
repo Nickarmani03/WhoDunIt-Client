6e30d557b94559617fd4b3b65e661ce392f4357b
Nickarmani03 <nickarmani03@gmail.com>
2021-09-13 13:51:22 -0500

    whodunit.js for paths

--- whodunit/src/index.js
+++ whodunit/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom"
-// import { Whodunit } from "./components/Whodunit.js"
+import { Whodunit } from "./components/Whodunit.js"
 import "./index.css"
 
 ReactDOM.render(
